a13b5cf3d4972eb7bc3068115505ad10f9aab962
blainefreestone <blainefreestone01@gmail.com>
2024-01-17 12:02:53 -0700

    get_player_batting_statistics returns table with batting average, on base percentage, and slugging percentage.

--- tables.py
+++ tables.py
@@ -11,7 +11,7 @@ def get_all_players_batting_data():
     return pd.merge(master_data, \
                     batting_data, \
                     on=["playerID"]) \
-                    [["playerID", "nameLast", "nameFirst", "yearID", "AB", "H", "2B", "3B", "HR", "BB", "SO"]] \
+                    [["playerID", "nameLast", "nameFirst", "yearID", "AB", "H", "2B", "3B", "HR", "BB", "HBP", "SO", "SF"]] \
                     .sort_values(by=["yearID", "nameLast", "nameFirst"]) \
                     .dropna()
 
@@ -21,6 +21,22 @@ def get_player_batting_data(player_id):
 
 # Returns pandas DataFrame with league-wide 
 def get_league_batting_data():
-    return get_all_players_batting_data()[["yearID", "AB", "H", "2B", "3B", "HR", "BB", "SO"]] \
+    return get_all_players_batting_data()[["yearID", "AB", "H", "2B", "3B", "HR", "BB", "HBP", "SO", "SF"]] \
                                          .groupby("yearID") \
                                          .sum()
+
+def get_player_batting_statistics(player_batting_data):
+    hits = player_batting_data.H
+    at_bats = player_batting_data.AB
+    base_on_balls = player_batting_data.BB
+    hit_by_pitch = player_batting_data.HBP
+    sacrifice_flies = player_batting_data.SF
+    doubles = player_batting_data["2B"]
+    triples = player_batting_data["3B"]
+    home_runs = player_batting_data.HR
+    singles = hits - doubles - triples - home_runs
+
+    player_batting_data["AVG"] = hits / at_bats
+    player_batting_data["OBP"] = (hits + base_on_balls + hit_by_pitch) / (at_bats + base_on_balls + hit_by_pitch + sacrifice_flies)
+    player_batting_data["SLG"] = ((singles) + (doubles * 2) + (triples * 3) + (home_runs * 4)) / at_bats
+    return player_batting_data[["playerID", "nameLast", "nameFirst", "yearID", "AVG", "OBP", "SLG"]]
