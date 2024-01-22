ac92c29ae0081d4306dc40aff28eef77ba0bf9d0
blainefreestone <blainefreestone01@gmail.com>
2024-01-22 13:02:57 -0700

    Add titles to graphs with league or player name

--- BattingDataDisplay.py
+++ BattingDataDisplay.py
@@ -9,12 +9,14 @@ class BattingDataDisplay:
     def graph_player_batting_statistics(self, player_id):
         player_data = self.batting_data.statistics_for_player(player_id)
         player_data.plot(marker="o", linestyle="dashed")
+        plt.title(f"{self.batting_data.name_for_player(player_id)} Statistics")
         plt.xlabel("Season")
         plt.show()
 
     def graph_league_batting_statistics(self):
         league_data = self.batting_data.statistics_for_league()
         league_data.plot(marker="none")
+        plt.title("MLB League Statistics 1871-2015")
         plt.xlabel("Season")
         plt.show()
 
