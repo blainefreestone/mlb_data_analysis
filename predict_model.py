766981d0e2c4a0a01e0c9996e33f8be718b30cbc
blainefreestone <blainefreestone01@gmail.com>
2024-01-23 20:51:10 -0700

    Predict model generates predictions for 10 random players in 2015 and displays the comparison.

---
+++ predict_model.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+from BattingData import BattingData
+
+from xgboost import XGBRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+
+# Create x and y sets
+batting_data = BattingData()
+X = batting_data.for_predict_model()[["age", "AB", "AVG", "careerAVG"]]
+X.dropna(axis=0, subset=["AVG"], inplace=True)
+y = X['AVG']
+X.drop(['AVG'], axis=1, inplace=True)
+
+# Generate and fit the predictive model
+gradient_model = XGBRegressor(random_state=0, n_estimators=500, learning_rate=0.04)
+gradient_model.fit(X, y)
+
+# Generate input data for random model for 10 randomly selected players in 2015
+playerIDs = ['morelmi01', 'larocad01', 'beckhti01', 'giavojo01', 'swihabl01', 'gomezca01', 'loneyja01', 'crawfbr01', 'gardnbr01', 'bautijo02']
+year = 2015
+mask = batting_data.for_predict_model()["playerID"].isin(playerIDs)
+batting_data_2015 = batting_data.for_predict_model()[mask].query(f"yearID == {year}")
+print(batting_data_2015)
+
+# Generate predictions with the model
+predictions = pd.Series(gradient_model.predict(batting_data_2015[["age", "AB", "careerAVG"]]))
+
+# Put predictions with their players and information
+output = pd.DataFrame(
+    {
+        "playerID": batting_data_2015["playerID"].to_list(),
+        "nameFirst": batting_data_2015["nameFirst"].to_list(),
+        "nameLast": batting_data_2015["nameLast"].to_list(),
+        "real_AVG": batting_data_2015["AVG"].to_list(),
+        "predicted_AVG": predictions,
+    }
+)
